a5b22464a6bd4136c70fc1207bf425c9a0801af6
Phataratah Sa-nguannarm <51017311+pattywan234@users.noreply.github.com>
2021-06-06 14:00:42 +0900

    Add files via upload

--- LSTM_model06.py
+++ LSTM_model06.py
@@ -2,6 +2,9 @@
 import numpy as np
 import tensorflow as tf
 import pickle
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import (recall_score, accuracy_score, f1_score, precision_score)
+import pandas as pd
 
 X_train = np.load('wesad/all_subjects/24May2021/X_train.npy')
 X_test = np.load('wesad/all_subjects/24May2021/X_test.npy')
@@ -15,7 +18,7 @@ N_CLASSES = 4  # 7
 N_HIDDEN_UNITS = 13
 N_TIME_STEPS = 100
 N_FEATURES = 13
-
+epoch = 50
 
 class CustomLSTM(tf.keras.Model):
     def __init__(self, **kwargs):
@@ -41,23 +44,47 @@ class CustomLSTM(tf.keras.Model):
 
 
 # history = []
+f1, prec, recall, acc, ROC_AUC, conf = ([], [], [], [], [], [])
+metric_cols = ['f1', 'precision', 'recall', 'accuracy', 'roc_auc']
+
 
 model = CustomLSTM()
 
 # loss_fn = tf.keras.losses.CategoricalCrossentropy()
 opt = tf.keras.optimizers.Adamax(learning_rate=0.001)
 
-
+train_x, val_x, train_y, val_y = train_test_split(X_train, y_train, test_size=0.25, random_state=42)
 def custom_loss(y_true, y_pred):
     return tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=y_pred, labels=y_true))
 
 
 model.compile(optimizer=opt, loss=custom_loss, metrics=['accuracy'])
 
-history_constant = model.fit(X_train, y_train, epochs=50, batch_size=1024, verbose=1, validation_data=(X_test, y_test))
+history_constant = model.fit(train_x, train_y, epochs=epoch, batch_size=1024, verbose=1, validation_data=(val_x, val_y))
 
+predicted = model.predict(X_test)
+
+max_predict = np.argmax(predicted, axis=1)
+max_y = np.argmax(y_test, axis=1)
+
+f1.append(f1_score(max_y, max_predict, average='weighted'))
+prec.append(precision_score(max_y, max_predict, average='weighted'))
+recall.append(recall_score(max_y, max_predict, average='weighted'))
+acc.append(accuracy_score(max_y, max_predict))
+#
+#performance = pd.DataFrame(zip(f1, prec, recall, acc, ROC_AUC, conf), columns=metric_cols).to_numpy()
+# print(performance)
+#np.save('wesad/all_subjects/24May2021/performance_matrix.npy', performance)
+
+# evaluate_model = model.evaluate(predicted, y_test)
 # history.append(history_constant)
 
+#np.save('wesad/all_subjects/24May2021/predicted_keras.npy', predicted)
+# np.save('wesad/all_subjects/24May2021/evaluate_keras.npy', evaluate_model)
+np.save(f'wesad/all_subjects/24May2021/new_model/f1_model06_{epoch}', f1)
+np.save(f'wesad/all_subjects/24May2021/new_model/precision_model06_{epoch}', prec)
+np.save(f'wesad/all_subjects/24May2021/new_model/recall_model06_{epoch}', recall)
+np.save(f'wesad/all_subjects/24May2021/new_model/accuracy_model06_{epoch}', acc)
 # np.save('wesad/all_subjects/24May2021/history.npy', history)
-with open('wesad/all_subjects/24May2021/histopy_sttdev.pkl', 'wb') as file_pi:
+with open('wesad/all_subjects/24May2021/histopy.pkl', 'wb') as file_pi:
     pickle.dump(history_constant.history, file_pi)
--- LSTM_model07.py
+++ LSTM_model07.py
@@ -1,10 +1,12 @@
 # k-fold cross validation, when k=10
+import keras.callbacks
 import numpy as np
 import tensorflow as tf
 from sklearn.model_selection import KFold
 import pandas as pd
 from sklearn.metrics import (recall_score, accuracy_score, f1_score, precision_score)
-
+import pickle
+import os.path
 
 data_1 = np.load('wesad/all_subjects/24May2021/extracted_all_1.npy')
 data_2 = np.load('wesad/all_subjects/24May2021/extracted_all_2.npy')
@@ -33,6 +35,9 @@ for i in range(0, len(all_data) - N_TIME_STEPS, step):
 reshaped_segments = np.asarray(segments, dtype=np.float32).reshape(-1, N_TIME_STEPS, N_FEATURES)
 labeld = np.asarray(pd.get_dummies(labelss), dtype=np.float32)
 
+print('input shape:', reshaped_segments.shape)
+print('target shape: ', labeld.shape)
+
 N_CLASSES = 4 #7
 N_HIDDEN_UNITS = 13
 
@@ -41,11 +46,11 @@ class CustomLSTM(tf.keras.Model):
     def __init__(self, **kwargs):
         super(CustomLSTM, self).__init__(**kwargs)
 
-    def build(self, N_HIDDEN_UNITS):
-        self.w_hidden = tf.Variable(tf.random.normal([N_FEATURES, N_HIDDEN_UNITS], stddev=0.01))
-        self.w_output = tf.Variable(tf.random.normal([N_HIDDEN_UNITS, N_CLASSES], stddev=0.01))
-        self.b_hidden = tf.Variable(tf.random.normal([N_HIDDEN_UNITS], stddev=0.01))
-        self.b_output = tf.Variable(tf.random.normal([N_CLASSES], stddev=0.01))
+    def build(self, num_of_lstms):
+        self.w_hidden = tf.Variable(tf.random.normal([N_FEATURES, N_HIDDEN_UNITS], mean=0.5))
+        self.w_output = tf.Variable(tf.random.normal([N_HIDDEN_UNITS, N_CLASSES], mean=0.5))
+        self.b_hidden = tf.Variable(tf.random.normal([N_HIDDEN_UNITS], mean=0.5))
+        self.b_output = tf.Variable(tf.random.normal([N_CLASSES], mean=0.5))
 
         self.lstm_layers = [tf.compat.v1.nn.rnn_cell.BasicLSTMCell(N_HIDDEN_UNITS, forget_bias=1.0) for _ in range(2)]
         self.lstm_layers = tf.compat.v1.nn.rnn_cell.MultiRNNCell(self.lstm_layers)
@@ -59,24 +64,28 @@ class CustomLSTM(tf.keras.Model):
         lstm_last_output = outputs[-1]
         return tf.matmul(lstm_last_output, self.w_output) + self.b_output
 
-fold_no = 10
+fold_no = 2
 
-f1, prec, recall, acc, ROC_AUC, conf = ([], [], [], [], [], [])
-metric_cols = ['f1', 'precision', 'recall', 'accuracy', 'roc_auc']
+f1, prec, recall, acc = [], [], [], []
+metric_cols = ['f1', 'precision', 'recall', 'accuracy']
 cv_kfold = KFold(n_splits=fold_no, random_state=42, shuffle=True)
 
 model = CustomLSTM()
-model.summary()
+# model.summary()
 # print(type(model))
 # exit()
 
-history = []
+train_acc = []
+train_loss = []
+val_acc = []
+val_loss = []
 def custom_loss(y_true, y_pred):
-   return tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=y_pred, labels=y_true))
+   return tf.nn.sigmoid_cross_entropy_with_logits(logits=y_pred, labels=y_true)
 
 # loss_fn = tf.keras.losses.CategoricalCrossentropy()
+csv_logger = keras.callbacks.CSVLogger(os.path.join('/training.log'), separator=',' )
 opt = tf.keras.optimizers.Adam(learning_rate=0.001)
-
+performance_matrix = []
 for train_index, val_index in cv_kfold.split(reshaped_segments):
     train_x = reshaped_segments[train_index]
     train_y = labeld[train_index]
@@ -84,24 +93,37 @@ for train_index, val_index in cv_kfold.split(reshaped_segments):
     val_y = labeld[val_index]
 
     model.compile(optimizer=opt, loss=custom_loss, metrics=['accuracy'])
+    history_constant = model.fit(train_x, train_y, epochs=1, batch_size=1024, verbose=1, validation_data=(val_x, val_y), callbacks=[csv_logger])
 
-    history_constant = model.fit(train_x, train_y, epochs=10, batch_size=1024, verbose=1, validation_data=(val_x, val_y))
-    history.append(history_constant)
+    train_acc.append(history_constant.history['accuracy'])
+    train_loss.append(history_constant.history['loss'])
+    val_acc.append(history_constant.history['val_accuracy'])
+    val_loss.append(history_constant.history['val_loss'])
 
     pred_test = model.predict(val_x, verbose=1)
-    pred_test_probs = model.predict_proba(val_x)
-    # fpr, tpr, thresholds = roc_curve(val_y, pred_test_probs)
-    max_predict = np.argmax(pred_test_probs, axis=1)
+    # pred_test_probs = model.predict_proba(val_x)
+    # # fpr, tpr, thresholds = roc_curve(val_y, pred_test_probs)
+    max_predict = np.argmax(pred_test, axis=1)
     max_y = np.argmax(val_y, axis=1)
-
+    #
     f1.append(f1_score(max_y, max_predict, average='weighted'))
     prec.append(precision_score(max_y, max_predict, average='weighted'))
     recall.append(recall_score(max_y, max_predict, average='weighted'))
     acc.append(accuracy_score(max_y, max_predict))
+    #
+    #performance = pd.DataFrame(zip(f1, prec, recall, acc), columns=metric_cols).to_numpy()
+    #performance_matrix.append(performance)
 
-    performance = pd.DataFrame(zip(f1, prec, recall, acc, ROC_AUC, conf), columns=metric_cols)
-    print(performance)
-    performance.to_numpy('wesad/all_subjects/24May2021/performance_matrix_keras.npy')
+#np.save('wesad/all_subjects/24May2021/new_model/performance_matrix_kfold.npy', performance_matrix)
+#with open('wesad/all_subjects/24May2021/histopy_kfold.pkl', 'wb') as file_pi:
+    #pickle.dump(history_constant.history, file_pi)
+# np.save('wesad/all_subjects/24May2021/new_model/f1_10fold.npy', f1)
+# np.save('wesad/all_subjects/24May2021/new_model/precision_10fold.npy', prec)
+# np.save('wesad/all_subjects/24May2021/new_model/recall_10fold.npy', recall)
+# np.save('wesad/all_subjects/24May2021/new_model/accuracy_10fold.npy', acc)
+# np.save('wesad/all_subjects/24May2021/new_model/train_acc_kfold.npy', train_acc)
+# np.save('wesad/all_subjects/24May2021/new_model/train_loss_kfold.npy', train_loss)
+# np.save('wesad/all_subjects/24May2021/new_model/val_acc_kfold.npy', val_acc)
+# np.save('wesad/all_subjects/24May2021/new_model/val_loss_kfold.npy', val_loss)
 
-np.save('wesad/all_subjects/24May2021/history_constant.npy', history)
 
